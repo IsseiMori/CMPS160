f8eb5c66d3cc0dcb380c2976ce13fe7f7b4f13c7
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-18 15:43:14 -0800

    a

---
+++ practice/HelloCanvas.js
@@ -0,0 +1,16 @@
+function main(){
+	var canvas = document.getElementById('webgl');
+	if(!canvas){
+		console.log("failed to retrive <canvas> element");
+		return;
+	}
+
+	var gl = getWebGLContext(canvas);
+	if(!gl){
+		console.log("failed to get rendering context for WebGL");
+		return;
+	}
+	
+	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
