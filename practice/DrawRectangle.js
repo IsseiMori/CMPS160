d36bf3c6898a59b4e5d1a41a2f796186d5fa5843
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-18 15:22:27 -0800

    a

---
+++ practice/DrawRectangle.js
@@ -0,0 +1,12 @@
+function main(){
+	window.alert("aa");
+	var canvas = document.getElementById('example');
+	if(!canvas){
+		console.log("failed to retrive <canvas> element");
+		return;
+	}
+
+	var ctx = canvas.getContent('2d');
+	ctx.fillStyle = 'rgba(0,0,255,1.0)';
+	ctx.fillRect(120,10,150,150);
+}
