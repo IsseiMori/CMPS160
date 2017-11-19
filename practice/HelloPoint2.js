436a76bf8a1b07ce64579a1cc713a9863b1bbd80
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-19 14:05:24 -0800

    a

--- practice/HelloPoint2.js
+++ practice/HelloPoint2.js
@@ -37,7 +37,7 @@ function main(){
 
 	gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0);
 	
-	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.clearColor(0.5,0.5,0.5,1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	gl.drawArrays(gl.POINTS, 0, 1);
