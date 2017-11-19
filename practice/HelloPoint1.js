91bff47e4d464902a5a2cf091b94e93c82094ba0
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-19 03:48:40 -0800

    a

--- practice/HelloPoint1.js
+++ practice/HelloPoint1.js
@@ -1,3 +1,14 @@
+var VSHADER_SOURCE = 
+	'void main(){\n' + 
+	'	gl_Position = vec4(0.0,0.0,0.0,1.0);\n' +
+	'	gl_PointSize = 10.0;\n' + 
+	'}\n';
+
+var FSHADER_SOURCE = 
+	'void main(){\n' + 
+	'	gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n' +
+	'}\n';
+
 function main(){
 	var canvas = document.getElementById('webgl');
 	if(!canvas){
@@ -11,11 +22,14 @@ function main(){
 		console.log("failed to get rendering context for WebGL");
 		return;
 	}
+
+	if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+		console.log("Failed to initialize shaders");
+		return;
+	}
 	
 	gl.clearColor(0.5,0.5,0.5,1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
-	var ctx = canvas.getContext('2d');
-	ctx.fillStyle = 'rgba(0,0,255,1.0)';
-	ctx.fillRect(120,10,150,150);
+	gl.drawArrays(gl.POINTS, 0, 1);
 }
