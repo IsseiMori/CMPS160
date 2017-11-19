d406ddb8c0dfa60ce27e77ca1b8de583699a3489
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-19 14:22:21 -0800

    a

--- practice/HelloPoint2.js
+++ practice/HelloPoint2.js
@@ -1,7 +1,7 @@
 var VSHADER_SOURCE = 
 	'attribute vec4 a_Position;\n' +
 	'void main(){\n' + 
-	'	gl_Position = a_Position\n' +
+	'	gl_Position = a_Position;\n' +
 	'	gl_PointSize = 10.0;\n' + 
 	'}\n';
 
