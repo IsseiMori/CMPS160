1aeed45239c51ab774718b4ca6ec74fe127237e1
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-19 16:01:36 -0800

    a

--- practice/ClickedPoints.js
+++ practice/ClickedPoints.js
@@ -8,7 +8,7 @@ var VSHADER_SOURCE =
 
 var FSHADER_SOURCE = 
 	'void main(){\n' + 
-	'	gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n' +
+	'	gl_FragColor = vec4(1.0,1.0,0.0,1.0);\n' +
 	'}\n';
 
 function main(){
@@ -42,11 +42,36 @@ function main(){
 		return;
 	}
 
-	gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-	gl.vertexAttrib1f(a_PointSize, 20.0);
+	canvas.onmousedown = function(ev){
+		click(ev, gl, canvas, a_Position);
+	}
+
+	gl.vertexAttrib1f(a_PointSize, 10.0);
 	
 	gl.clearColor(0.5,0.5,0.5,1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
-	gl.drawArrays(gl.POINTS, 0, 1);
+var g_points = [];
+
+function click(ev, gl, canvas, a_Position){
+	var x = ev.clientX;
+	var y = ev.clientY;
+
+	var rect = ev.target.getBoundingClientRect();
+
+	x = ((x - rect.left) - (canvas.width)/2)/(canvas.width/2);
+	y = ((canvas.height)/2 - (y - rect.top))/(canvas.height/2);
+
+	g_points.push([x,y]);
+
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	var len = g_points.length;
+
+	for(var i = 0; i < len; i++){
+		var xy = g_points[i];
+		gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+		gl.drawArrays(gl.POINTS, 0, 1);
+	}
 }
