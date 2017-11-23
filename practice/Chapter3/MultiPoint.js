905ec7e22ea4dbd2938dbe43eca91a9f3be05921
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-23 01:04:36 -0800

    a

--- practice/Chapter3/MultiPoint.js
+++ practice/Chapter3/MultiPoint.js
@@ -1,9 +1,8 @@
 var VSHADER_SOURCE = 
 	'attribute vec4 a_Position;\n' +
-	'attribute float a_PointSize;\n' +
 	'void main(){\n' + 
 	'	gl_Position = a_Position;\n' +
-	'	gl_PointSize = a_PointSize;\n' + 
+	'	gl_PointSize = 10.0;\n' + 
 	'}\n';
 
 var FSHADER_SOURCE = 
@@ -30,48 +29,43 @@ function main(){
 		return;
 	}
 
+	var n = initVartexBuffers(gl);
+	if(n < 0){
+		console.log("failed to set the positions of the vertices");
+		return;
+	}
+
+	
+	gl.clearColor(0.5,0.5,0.5,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.drawArrays(gl.POINTS, 0, n);
+}
+
+function initVartexBuffers(gl){
+	var vertices = new Float32Array([
+		0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+		]);
+	var n = 3;
+
+	var vertexBuffer = gl.createBuffer();
+	if(!vertexBuffer){
+		console.log("failed to create the buffer object");
+		return -1;
+	}
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
 	var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
 	if(a_Position < 0){
 		console.log("failed to get the storage location of a_Position");
 		return;
 	}
 
-	var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-	if(a_PointSize < 0){
-		console.log("failed to get the storage location for a_PointSize");
-		return;
-	}
+	gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
 
-	canvas.onmousedown = function(ev){
-		click(ev, gl, canvas, a_Position);
-	}
+	gl.enableVertexAttribArray(a_Position);
 
-	gl.vertexAttrib1f(a_PointSize, 10.0);
-	
-	gl.clearColor(0.5,0.5,0.5,1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-}
-
-var g_points = [];
-
-function click(ev, gl, canvas, a_Position){
-	var x = ev.clientX;
-	var y = ev.clientY;
-
-	var rect = ev.target.getBoundingClientRect();
-
-	x = ((x - rect.left) - (canvas.width)/2)/(canvas.width/2);
-	y = ((canvas.height)/2 - (y - rect.top))/(canvas.height/2);
-
-	g_points.push([x,y]);
-
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	var len = g_points.length;
-
-	for(var i = 0; i < len; i++){
-		var xy = g_points[i];
-		gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-		gl.drawArrays(gl.POINTS, 0, 1);
-	}
+	return n;
 }
