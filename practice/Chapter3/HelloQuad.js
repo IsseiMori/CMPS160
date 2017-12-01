5345e96c86858b153f18b4ef20eaaf15579f5a92
IsseiMori <ganntyu1225issei@gmail.com>
2017-11-30 18:31:33 -0800

    a

---
+++ practice/Chapter3/HelloQuad.js
@@ -0,0 +1,70 @@
+var VSHADER_SOURCE = 
+	'attribute vec4 a_Position;\n' +
+	'void main(){\n' + 
+	'	gl_Position = a_Position;\n' +
+	'}\n';
+
+var FSHADER_SOURCE = 
+	'void main(){\n' + 
+	'	gl_FragColor = vec4(1.0,1.0,0.0,1.0);\n' +
+	'}\n';
+
+function main(){
+	var canvas = document.getElementById('webgl');
+	if(!canvas){
+		console.log("failed to retrive <canvas> element");
+		return;
+	}
+
+	var gl = getWebGLContext(canvas);
+
+	if(!gl){
+		console.log("failed to get rendering context for WebGL");
+		return;
+	}
+
+	if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+		console.log("Failed to initialize shaders");
+		return;
+	}
+
+	var n = initVartexBuffers(gl);
+	if(n < 0){
+		console.log("failed to set the positions of the vertices");
+		return;
+	}
+
+	
+	gl.clearColor(0.5,0.5,0.5,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+}
+
+function initVartexBuffers(gl){
+	var vertices = new Float32Array([
+		-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5
+		]);
+	var n = 4;
+
+	var vertexBuffer = gl.createBuffer();
+	if(!vertexBuffer){
+		console.log("failed to create the buffer object");
+		return -1;
+	}
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+	var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+	if(a_Position < 0){
+		console.log("failed to get the storage location of a_Position");
+		return;
+	}
+
+	gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+	gl.enableVertexAttribArray(a_Position);
+
+	return n;
+}
