bca12bf225c1a4d03c99f947da9d9790c2cba2c1
IsseiMori <ganntyu1225issei@gmail.com>
2017-12-22 00:54:49 -0800

    modified TexturedQuad to TexuturedQuad_Clamp_Mirror

--- practice/Chapter5/TexturedQuad.js
+++ practice/Chapter5/TexturedQuad.js
@@ -48,10 +48,10 @@ function main(){
 
 function initVartexBuffers(gl){
 	var verticesTexCoords = new Float32Array([
-		-0.5, 0.5, 0.0, 1.0,
-		-0.5, -0.5, 0.0, 0.0,
-		0.5, 0.5, 1.0, 1.0,
-		0.5, -0.5, 1.0, 0.0
+		-0.5,   0.5,  -0.3,   1.7,
+		-0.5,  -0.5,   -0.3,  -0.2,
+		 0.5,   0.5,   1.7,   1.7,
+		 0.5,  -0.5,   1.7,  -0.2
 		]);
 	var n = 4;
 
@@ -116,11 +116,15 @@ function loadTexture(gl, n, texture, u_Sampler, image) {
 	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
 	gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, texture);
+
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
 	gl.uniform1i(u_Sampler, 0);
 
-	gl.clear(0.5,0.5,0.5, 1.0);
+	gl.clearColor(0.5,0.5,0.5, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
 }
