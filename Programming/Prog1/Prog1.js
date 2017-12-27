f19f3607e08b16e15bc8717e1b8d14477152f7f9
IsseiMori <ganntyu1225issei@gmail.com>
2017-12-27 01:57:42 -0800

    Prog1 modify previous circle based on current and prevprev circle, angle bug not fixed

--- Programming/Prog1/Prog1.js
+++ Programming/Prog1/Prog1.js
@@ -49,6 +49,7 @@ function main(){
 	gl.drawArrays(gl.LINE_STRIP, 0, vertices.length / 3);
 }
 
+//make an array of 12 sided circle located at origin
 var circle = [];
 function makeCircle(canvas){
 	var r;
@@ -64,22 +65,70 @@ function makeCircle(canvas){
 }
 
 var prevAngle;
-var vertices = [0.0,0.0,0.0,1.0,1.0,1.0];
+var prevprevAngle;
+var vertices = []; //center point and 12 vertices -> pair of 39 vertices
+var prevPoint = [];
 function click(ev, gl, canvas, a_Position){
+	//convert canvas coordinate to webgl coordinate
 	var x = ev.clientX;
 	var y = ev.clientY;
 	var rect = ev.target.getBoundingClientRect();
 	x = ((x - rect.left) - (canvas.width)/2)/(canvas.width/2);
 	y = ((canvas.height)/2 - (y - rect.top))/(canvas.height/2);
-	console.log(circle);
 
-	for(i = 0; i < 36; i+=3){
-		vertices.push(circle[i]+x);
-		vertices.push(circle[i+1]+y);
-		vertices.push(circle[i+2]);
+	var circleRotateMatrix = new Matrix4();
+	var v3;
+	var tmpPoints = [];
+	var tmp = [];
+	var angle;
+
+	//move the circle to the clicked point and push vertices to vertices array
+	if(prevAngle == null){
+		prevAngle = 0;
+	}else{
+		//calculate the angle between current point and previous point
+		prevAngle = 180 / Math.PI * -Math.atan2(x - prevPoint[0], y - prevPoint[1]);
+		//change the rotation of previous circle based on prevprevAngle and the current angle
+		if(prevprevAngle != null){
+			prevAngle = (prevAngle + prevprevAngle) / 2.0;
+		}
+		circleRotateMatrix.setRotate(prevAngle, 0, 0, 1);
+		for(i = -36; i < 0; i+=3){
+			tmpPoints[0] = vertices[vertices.length + i] - vertices[vertices.length - 39];
+			tmpPoints[1] = vertices[vertices.length + i + 1] - vertices[vertices.length - 38];
+			tmpPoints[2] = vertices[vertices.length + i + 2] - vertices[vertices.length - 37];
+			v3 = new Vector3(tmpPoints);
+			v3 = circleRotateMatrix.multiplyVector3(v3);
+			tmp = new Float32Array(v3.elements);
+			vertices[vertices.length + i] = tmp[0] + vertices[vertices.length - 39];
+			vertices[vertices.length + i + 1] = tmp[1] + vertices[vertices.length - 38];
+			vertices[vertices.length + i + 2] = tmp[2] + vertices[vertices.length - 37];
+		}
 	}
 
-	console.log(vertices);
+	vertices.push(x);
+	vertices.push(y);
+	vertices.push(0);
+
+	angle = 180 / Math.PI * -Math.atan2(x - prevPoint[0], y - prevPoint[1]);;
+	circleRotateMatrix.setRotate(angle, 0, 0, 1);
+	for(i = 0; i < 36; i+=3){
+		tmpPoints[0] = circle[i];
+		tmpPoints[1] = circle[i+1];
+		tmpPoints[2] = circle[i+2];
+		v3 = new Vector3(tmpPoints);
+		v3 = circleRotateMatrix.multiplyVector3(v3);
+		tmp = new Float32Array(v3.elements);
+		vertices.push(tmp[0]+x);
+		vertices.push(tmp[1]+y);
+		vertices.push(tmp[2]);
+	}
+
+	prevPoint[0] = x;
+	prevPoint[1] = y;
+
+	prevprevAngle = prevAngle;
+	prevAngle = angle;
 
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
