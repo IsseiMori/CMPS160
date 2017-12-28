30069152480ca6884bd7f0b4a1be4168089902fd
IsseiMori <ganntyu1225issei@gmail.com>
2017-12-28 14:58:43 -0800

    added LightedCube

---
+++ practice/Chapter8/LightedCube.js
@@ -0,0 +1,154 @@
+var VSHADER_SOURCE = 
+	'attribute vec4 a_Position;\n' +
+	'attribute vec4 a_Color;\n' +
+	'attribute vec4 a_Normal;\n' +
+	'uniform mat4 u_MvpMatrix;\n' +
+	'uniform vec3 u_LightColor;\n' +
+	'uniform vec3 u_LightDirection;\n' +
+	'varying vec4 v_Color;\n' +
+	'void main(){\n' + 
+	'	gl_Position = u_MvpMatrix * a_Position;\n' +
+	'	vec3 normal = normalize(vec3(a_Normal));\n' +
+	'	float nDotL = max(dot(u_LightDirection, normal), 0.0);\n' +
+	'	vec3 diffuse = u_LightColor * vec3(a_Color) * nDotL;\n' + 
+	'	v_Color = vec4(diffuse, a_Color.a);\n' + 
+	'}\n';
+
+var FSHADER_SOURCE = 
+	'precision mediump float;\n' +
+	'varying vec4 v_Color;\n' +
+	'void main(){\n' + 
+	'	gl_FragColor = v_Color;\n' +
+
+	'}\n';
+
+function main(){
+	var canvas = document.getElementById('webgl');
+	if(!canvas){
+		console.log("failed to retrive <canvas> element");
+		return;
+	}
+
+	var gl = getWebGLContext(canvas);
+
+	if(!gl){
+		console.log("failed to get rendering context for WebGL");
+		return;
+	}
+
+	if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+		console.log("Failed to initialize shaders");
+		return;
+	}
+
+	var n = initVertexBuffers(gl);
+	if(n < 0){
+		console.log("failed to set the positions of the vertices");
+		return;
+	}
+
+	var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+	var u_LightColor= gl.getUniformLocation(gl.program, 'u_LightColor');
+	var u_LightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
+	if(u_MvpMatrix < 0 || u_LightColor < 0 || u_LightDirection < 0){
+		console.log("failed to get the storage location of u_ attribute");
+		return;
+	}
+
+	//set light color to white
+	gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
+
+	var lightDirection = new Vector3([0.5, 3.0, 4.0]);
+	lightDirection.normalize();
+	gl.uniform3fv(u_LightDirection, lightDirection.elements);
+
+	var projMatrix = new Matrix4();
+	var viewMatrix = new Matrix4();
+	var modelMatrix = new Matrix4();
+	var mvpMatrix = new Matrix4();
+
+	//modelMatrix.setTranslate(0.75, 0.0, 0.0);
+	viewMatrix.setLookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+	projMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
+
+	mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
+	gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+	gl.clearColor(0.5,0.5,0.5, 1.0);
+	gl.enable(gl.DEPTH_TEST);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+}
+
+function initVertexBuffers(gl){
+	// Create a cube
+	//    v6----- v5
+	//   /|      /|
+	//  v1------v0|
+	//  | |     | |
+	//  | |v7---|-|v4
+	//  |/      |/
+	//  v2------v3
+
+	var vertices = new Float32Array([   // Vertex coordinates
+    	 1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+	     1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+	     1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+	    -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+	    -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+	     1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+	]);
+
+	var colors = new Float32Array([    // Colors
+	   1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v1-v2-v3 front
+	   1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v3-v4-v5 right
+	   1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v5-v6-v1 up
+	   1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v1-v6-v7-v2 left
+	   1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v7-v4-v3-v2 down
+	   1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0    // v4-v7-v6-v5 back
+	]);
+
+	var normals = new Float32Array([    // Normal
+	  0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+	  1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+	  0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+	 -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+	  0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+	  0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
+	]);
+
+	var indices = new Uint8Array([       // Indices of the vertices
+	     0, 1, 2,   0, 2, 3,    // front
+	     4, 5, 6,   4, 6, 7,    // right
+	     8, 9,10,   8,10,11,    // up
+	    12,13,14,  12,14,15,    // left
+	    16,17,18,  16,18,19,    // down
+	    20,21,22,  20,22,23     // back
+	]);
+
+	var indexBuffer = gl.createBuffer();
+	if(!indexBuffer){
+		console.log("failed to create the buffer object");
+		return -1;
+	}
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+	if(!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position')) return -1;
+	if(!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color')) return -1;
+	if(!initArrayBuffer(gl, normals, 3, gl.FLOAT, 'a_Normal')) return -1;
+
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+	return indices.length;
+}
+
+function initArrayBuffer(gl, data, num, type, attribute){
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+	var a_attribute = gl.getAttribLocation(gl.program, attribute);
+	gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+	gl.enableVertexAttribArray(a_attribute);
+
+	return true;
+}
