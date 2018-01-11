cefe78fd9a63349edbab5a632357969e22cfc2f2
IsseiMori <ganntyu1225issei@gmail.com>
2018-01-11 15:30:42 -0800

    added Prog2 files initial commit

--- Programming/Prog2/Prog2.js
+++ Programming/Prog2/Prog2.js
@@ -1,12 +1,19 @@
 var VSHADER_SOURCE = 
 	'attribute vec4 a_Position;\n' +
+	'attribute vec4 a_Normal;\n' +
+	'uniform vec3 u_LightDirection;\n' +
+	'varying vec4 v_Color;\n' +
 	'void main(){\n' + 
 	'	gl_Position = a_Position;\n' +
+	'	float nDotL = max(dot(u_LightDirection, normalize(a_Normal.xyz)),0.0);\n' +
+	'	v_Color = vec4(vec3(0.0, 1.0, 0.0) * nDotL, 1.0);\n' +
 	'}\n';
 
 var FSHADER_SOURCE = 
+	'precision mediump float;\n' +
+	'varying vec4 v_Color;\n' +
 	'void main(){\n' + 
-	'	gl_FragColor = vec4(0.0,0.0,0.0,1.0);\n' +
+	'	gl_FragColor = v_Color;\n' +
 	'}\n';
 
 function main(){
@@ -28,35 +35,64 @@ function main(){
 		return;
 	}
 
+	gl.enable(gl.DEPTH_TEST);
+
+	//initialize buffers
 	var indexBuffer = gl.createBuffer();
 	var verticesBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+	var normalBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
+	//get Shader attributes
 	var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+	var a_Normal = gl.getAttribLocation(gl.program, 'a_Normal');
+	var u_LightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
+	if(u_LightDirection < 0){
+		console.log("failed to get the storage location");
+		return;
+	}
+
+	//set lightDirection
+	var lightDirection = new Vector3([0.5, -2.0, 4.0]);
+	lightDirection.normalize();
+	gl.uniform3fv(u_LightDirection, lightDirection.elements);
+
 	makeCircle(canvas);
 
 	canvas.onmousedown = function(ev){
-		click(ev, gl, canvas, a_Position);
+		click(ev, gl, canvas, verticesBuffer, normalBuffer, a_Position, a_Normal);
 	}
 
 	gl.clearColor(0.5,0.5,0.5,1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	//gl.drawArrays(gl.LINE_STRIP, 0, vertices.length / 3);
 }
 
 //make an array of 12 sided circle located at origin
 var circle = [];
 function makeCircle(canvas){
+	//2 vertices for each side
+	//72 vertices in total
 	var r;
 	var rad = Math.PI / 180.0;
 	if(canvas.width < canvas.height) r = canvas.width * 0.05;
 	else r = canvas.height * 0.05;
 
 	for(i = 0; i < 36; i+=3){
-		circle[i] = 0.05 * Math.sin(30.0 * (i/3) * rad);
-		circle[i+1] = 0;
-		circle[i+2] = 0.05 * Math.cos(30 * (i/3) * rad);
+		circle.push(0.05 * Math.sin(30.0 * (i/3) * rad));
+		circle.push(0.0);
+		circle.push(0.05 * Math.cos(30 * (i/3) * rad));
+		//copy coordinate for next side
+		if(i > 0){
+			circle.push(0.05 * Math.sin(30.0 * (i/3) * rad));
+			circle.push(0.0);
+			circle.push(0.05 * Math.cos(30 * (i/3) * rad));
+		}
+		if(i == 33){
+			circle.push(circle[0]);
+			circle.push(circle[1]);
+			circle.push(circle[2]);
+		}
 	}
 }
 
@@ -65,8 +101,15 @@ var prevprevAngle;
 var vertices = []; //center point and 12 vertices -> pair of 39 vertices
 var prevPoint = [];
 var indices = [];
+//  48---49 26---27
+//   |   |   |   |
+//   |___|   |___|
+//	23   24  1    2
+//
+//
+var normals = [];
 
-function click(ev, gl, canvas, a_Position){
+function click(ev, gl, canvas, verticesBuffer, normalBuffer, a_Position, a_Normal){
 	//convert canvas coordinate to webgl coordinate
 	var x = ev.clientX;
 	var y = ev.clientY;
@@ -90,16 +133,25 @@ function click(ev, gl, canvas, a_Position){
 		//change the rotation of previous circle based on prevprevAngle and the current angle
 		circleRotateMatrix.setRotate((angle + prevprevAngle) / 2.0 - prevAngle , 0, 0, 1);
 		prevAngle = (angle + prevprevAngle) / 2.0;
-		for(i = -36; i < 0; i+=3){
-			tmpPoints[0] = vertices[vertices.length + i] - vertices[vertices.length - 39];
-			tmpPoints[1] = vertices[vertices.length + i + 1] - vertices[vertices.length - 38];
-			tmpPoints[2] = vertices[vertices.length + i + 2] - vertices[vertices.length - 37];
+		for(i = -75; i < 0; i+=3){
+			//calculate object coordinate
+			tmpPoints[0] = vertices[vertices.length + i] - vertices[vertices.length - 75];
+			tmpPoints[1] = vertices[vertices.length + i + 1] - vertices[vertices.length - 74];
+			tmpPoints[2] = vertices[vertices.length + i + 2] - vertices[vertices.length - 73];
 			v3 = new Vector3(tmpPoints);
 			v3 = circleRotateMatrix.multiplyVector3(v3);
 			tmp = new Float32Array(v3.elements);
-			vertices[vertices.length + i] = tmp[0] + vertices[vertices.length - 39];
-			vertices[vertices.length + i + 1] = tmp[1] + vertices[vertices.length - 38];
-			vertices[vertices.length + i + 2] = tmp[2] + vertices[vertices.length - 37];
+			vertices[vertices.length + i] = tmp[0] + vertices[vertices.length - 75];
+			vertices[vertices.length + i + 1] = tmp[1] + vertices[vertices.length - 74];
+			vertices[vertices.length + i + 2] = tmp[2] + vertices[vertices.length - 73];
+		}
+
+		//copy the circle vertices for the next polygon
+		if(vertices.length > 75){
+			var currentLength = vertices.length;
+			for(i = -75; i < 0; i++){
+				vertices.push(vertices[currentLength + i]);
+			}
 		}
 	}
 
@@ -108,10 +160,9 @@ function click(ev, gl, canvas, a_Position){
 	vertices.push(y);
 	vertices.push(0);
 
-	//modify the previous angle
+	//store new circle
 	circleRotateMatrix.setRotate(angle, 0, 0, 1);
-	console.log(angle, prevAngle);
-	for(i = 0; i < 36; i+=3){
+	for(i = 0; i < 72; i+=3){
 		tmpPoints[0] = circle[i];
 		tmpPoints[1] = circle[i+1];
 		tmpPoints[2] = circle[i+2];
@@ -125,21 +176,17 @@ function click(ev, gl, canvas, a_Position){
 	
 	//make polygon indeces
 	if(prevprevAngle != null){
-		for(i = 0; i < 11; i++){
-			indices.push(vertices.length / 3 - 26 + 14 + i);
-			indices.push(vertices.length / 3 - 26 + 1 + i); indices.push(vertices.length / 3 - 26 + 1 + i);
-			indices.push(vertices.length / 3 - 26 + 2 + i);
-			indices.push(vertices.length / 3 - 26 + 2 + i);
-			indices.push(vertices.length / 3 - 26 + 15 + i); indices.push(vertices.length / 3 - 26 + 15 + i);
-			indices.push(vertices.length / 3 - 26 + 14 + i);
+		for(i = 0; i < 24; i+=2){
+			indices.push(vertices.length / 3 - 50 + 26 + i);
+			indices.push(vertices.length / 3 - 50 + 1 + i);
+			indices.push(vertices.length / 3 - 50 + 2 + i);
+			indices.push(vertices.length / 3 - 50 + 2 + i);
+			indices.push(vertices.length / 3 - 50 + 27 + i);
+			indices.push(vertices.length / 3 - 50 + 26 + i);
 		}
-		indices.push(vertices.length / 3 - 26 + 25);
-		indices.push(vertices.length / 3 - 26 + 12); indices.push(vertices.length / 3 - 26 + 12);
-		indices.push(vertices.length / 3 - 26 + 1);
-		indices.push(vertices.length / 3 - 26 + 1);
-		indices.push(vertices.length / 3 - 26 + 14); indices.push(vertices.length / 3 - 26 + 14);
-		indices.push(vertices.length / 3 - 26 + 25);
 	}
+	//recalculate normals for the lastest 2 circles
+	calculateNormals();
  
  	//store the center point
 	prevPoint[0] = x;
@@ -149,14 +196,96 @@ function click(ev, gl, canvas, a_Position){
 	prevprevAngle = prevAngle;
 	prevAngle = angle;
 
+	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 	gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(a_Position);
-	//console.log(vertices);
-	//console.log(indices);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+	gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(a_Normal);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+	console.log(vertices);
+	console.log(indices);
+	console.log(normals);
 
 	gl.clearColor(0.5,0.5,0.5, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawElements(gl.LINES, indices.length, gl.UNSIGNED_BYTE, 0);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
 }
+
+function calculateNormals(){
+	
+	var x = []; //first vector
+	var y = []; //first vector
+	var n = []; //result normal vector
+	if(vertices.length > 75){
+		for(i = 3; i <= 69; i+=6){
+			x[0] = vertices[vertices.length - 150 + i] - vertices[vertices.length - 75 + i];
+			x[1] = vertices[vertices.length - 75 + 1 + i] - vertices[vertices.length - 75 + 1 + i];
+			x[2] = vertices[vertices.length - 75 + 2 + i] - vertices[vertices.length - 75 + 2 + i];
+			y[0] = vertices[vertices.length - 150 + 3 + i] - vertices[vertices.length - 150 + i];
+			y[1] = vertices[vertices.length - 150 + 4 + i] - vertices[vertices.length - 150 + 1 + i];
+			y[2] = vertices[vertices.length - 150 + 5 + i] - vertices[vertices.length - 150 + 2 + i];
+			n = crossProduct(x,y);
+			n[0] = -n[0];
+			n[1] = -n[1];
+			n[2] = -n[2];
+			normals[vertices.length - 150 + i] = n[0];
+			normals[vertices.length - 150 + i + 1] = n[1];
+			normals[vertices.length - 150 + i + 2] = n[2];
+			normals[vertices.length - 150 + i + 3] = n[0];
+			normals[vertices.length - 150 + i + 4] = n[1];
+			normals[vertices.length - 150 + i + 5] = n[2];
+			normals[vertices.length - 75 + i] = n[0];
+			normals[vertices.length - 75 + i + 1] = n[1];
+			normals[vertices.length - 75 + i + 2] = n[2];
+			normals[vertices.length - 75 + i + 3] = n[0];
+			normals[vertices.length - 75 + i + 4] = n[1];
+			normals[vertices.length - 75 + i + 5] = n[2];
+		}
+		if(vertices.length > 150){
+			for(i = 3; i <= 69; i+=6){
+				x[0] = vertices[vertices.length - 300 + i] - vertices[vertices.length - 225 + i];
+				x[1] = vertices[vertices.length - 225 + 1 + i] - vertices[vertices.length - 225 + 1 + i];
+				x[2] = vertices[vertices.length - 225 + 2 + i] - vertices[vertices.length - 225 + 2 + i];
+				y[0] = vertices[vertices.length - 300 + 3 + i] - vertices[vertices.length - 300 + i];
+				y[1] = vertices[vertices.length - 300 + 4 + i] - vertices[vertices.length - 300 + 1 + i];
+				y[2] = vertices[vertices.length - 300 + 5 + i] - vertices[vertices.length - 300 + 2 + i];
+				n = crossProduct(x,y);
+				n[0] = -n[0];
+				n[1] = -n[1];
+				n[2] = -n[2];
+				normals[vertices.length - 300 + i] = n[0];
+				normals[vertices.length - 300 + i + 1] = n[1];
+				normals[vertices.length - 300 + i + 2] = n[2];
+				normals[vertices.length - 300 + i + 3] = n[0];
+				normals[vertices.length - 300 + i + 4] = n[1];
+				normals[vertices.length - 300 + i + 5] = n[2];
+				normals[vertices.length - 225 + i] = n[0];
+				normals[vertices.length - 225 + i + 1] = n[1];
+				normals[vertices.length - 225 + i + 2] = n[2];
+				normals[vertices.length - 225 + i + 3] = n[0];
+				normals[vertices.length - 225 + i + 4] = n[1];
+				normals[vertices.length - 225 + i + 5] = n[2];
+			}
+		}
+	}
+}
+
+function crossProduct(x, y){
+	var result = [];
+	result[0] = x[1] * y[2] - x[2] * y[1];
+	result[1] = x[2] * y[0] - x[0] * y[2];
+	result[2] = x[0] * y[1] - x[1] * y[0];
+
+	return result;
+}
+
+
+
+
